512cd441634bed723a1f0baca023e6e6a3a9a967
Kashan Shaikh <shaikhkashan670@gmail.com>
2026-07-03 04:14:34 +0500

    s

--- resources/views/admin_panel/reports/claim_acceptance/preview.blade.php
+++ resources/views/admin_panel/reports/claim_acceptance/preview.blade.php
@@ -69,6 +69,32 @@
         }
         .data-row td { border: 1px solid #999; }
         .voucher-cell { font-weight: bold; text-align: center; }
+        .wh-icon {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            width: 16px;
+            height: 16px;
+            border-radius: 50%;
+            font-size: 13px;
+            font-weight: bold;
+            line-height: 1;
+            margin-right: 4px;
+            vertical-align: middle;
+            flex-shrink: 0;
+        }
+        .wh-minus {
+            color: #fff;
+            background-color: #c62828;
+        }
+        .wh-plus {
+            color: #fff;
+            background-color: #2e7d32;
+        }
+        .wh-cell {
+            display: flex;
+            align-items: center;
+        }
         .total-row td {
             font-weight: bold;
             border: 1px solid #000;
@@ -120,8 +146,8 @@
     <thead>
         <tr>
             <th width="8%">Voucher</th>
-            <th width="12%">Claim From</th>
-            <th width="12%">Accept In</th>
+            <th width="12%"><span class="wh-icon wh-minus">−</span> Claim From</th>
+            <th width="12%"><span class="wh-icon wh-plus">+</span> Accept In</th>
             <th width="9%">BTR #</th>
             <th width="9%">Date</th>
             <th width="20%">Supplier</th>
@@ -157,8 +183,8 @@
                 @endphp
                 <tr class="data-row">
                     <td class="voucher-cell">{{ $displayVoucher }}</td>
-                    <td class="text-left">{{ $claimFrom }}</td>
-                    <td class="text-left">{{ $acceptIn }}</td>
+                    <td class="text-left"><span class="wh-cell"><span class="wh-icon wh-minus">−</span>{{ $claimFrom }}</span></td>
+                    <td class="text-left"><span class="wh-cell"><span class="wh-icon wh-plus">+</span>{{ $acceptIn }}</span></td>
                     <td class="text-center">{{ $item->btr_no ?: '-' }}</td>
                     <td class="text-center">{{ $itemDate }}</td>
                     <td class="text-left">{{ $partyName }}</td>
